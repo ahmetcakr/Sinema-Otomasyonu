8f5ea3873b0f5c9944ae9d3dab2daf89073a51d0
ahmetcakr <ahmet.ckr720@gmail.com>
2022-02-13 20:11:12 +0300

    Koltuklar eklendi.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,295 @@ namespace SinemaOtomasyonu
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_chair01 = new System.Windows.Forms.Button();
+            this.btn_chair02 = new System.Windows.Forms.Button();
+            this.btn_chair03 = new System.Windows.Forms.Button();
+            this.btn_chair04 = new System.Windows.Forms.Button();
+            this.btn_chair05 = new System.Windows.Forms.Button();
+            this.btn_chair10 = new System.Windows.Forms.Button();
+            this.btn_chair09 = new System.Windows.Forms.Button();
+            this.btn_chair08 = new System.Windows.Forms.Button();
+            this.btn_chair07 = new System.Windows.Forms.Button();
+            this.btn_chair06 = new System.Windows.Forms.Button();
+            this.btn_chair15 = new System.Windows.Forms.Button();
+            this.btn_chair14 = new System.Windows.Forms.Button();
+            this.btn_chair13 = new System.Windows.Forms.Button();
+            this.btn_chair12 = new System.Windows.Forms.Button();
+            this.btn_chair11 = new System.Windows.Forms.Button();
+            this.btn_chair20 = new System.Windows.Forms.Button();
+            this.btn_chair19 = new System.Windows.Forms.Button();
+            this.btn_chair18 = new System.Windows.Forms.Button();
+            this.btn_chair17 = new System.Windows.Forms.Button();
+            this.btn_chair16 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_chair01
+            // 
+            this.btn_chair01.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair01.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair01.BackgroundImage")));
+            this.btn_chair01.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair01.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair01.Location = new System.Drawing.Point(150, 115);
+            this.btn_chair01.Name = "btn_chair01";
+            this.btn_chair01.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair01.TabIndex = 0;
+            this.btn_chair01.UseVisualStyleBackColor = false;
+            this.btn_chair01.Click += new System.EventHandler(this.btn_chair01_Click);
+            // 
+            // btn_chair02
+            // 
+            this.btn_chair02.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair02.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair02.BackgroundImage")));
+            this.btn_chair02.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair02.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair02.Location = new System.Drawing.Point(250, 115);
+            this.btn_chair02.Name = "btn_chair02";
+            this.btn_chair02.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair02.TabIndex = 1;
+            this.btn_chair02.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair03
+            // 
+            this.btn_chair03.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair03.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair03.BackgroundImage")));
+            this.btn_chair03.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair03.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair03.Location = new System.Drawing.Point(350, 115);
+            this.btn_chair03.Name = "btn_chair03";
+            this.btn_chair03.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair03.TabIndex = 2;
+            this.btn_chair03.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair04
+            // 
+            this.btn_chair04.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair04.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair04.BackgroundImage")));
+            this.btn_chair04.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair04.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair04.Location = new System.Drawing.Point(450, 115);
+            this.btn_chair04.Name = "btn_chair04";
+            this.btn_chair04.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair04.TabIndex = 3;
+            this.btn_chair04.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair05
+            // 
+            this.btn_chair05.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair05.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair05.BackgroundImage")));
+            this.btn_chair05.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair05.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair05.Location = new System.Drawing.Point(550, 115);
+            this.btn_chair05.Name = "btn_chair05";
+            this.btn_chair05.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair05.TabIndex = 4;
+            this.btn_chair05.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair10
+            // 
+            this.btn_chair10.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair10.BackgroundImage")));
+            this.btn_chair10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair10.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair10.Location = new System.Drawing.Point(550, 211);
+            this.btn_chair10.Name = "btn_chair10";
+            this.btn_chair10.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair10.TabIndex = 9;
+            this.btn_chair10.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair09
+            // 
+            this.btn_chair09.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair09.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair09.BackgroundImage")));
+            this.btn_chair09.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair09.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair09.Location = new System.Drawing.Point(450, 211);
+            this.btn_chair09.Name = "btn_chair09";
+            this.btn_chair09.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair09.TabIndex = 8;
+            this.btn_chair09.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair08
+            // 
+            this.btn_chair08.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair08.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair08.BackgroundImage")));
+            this.btn_chair08.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair08.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair08.Location = new System.Drawing.Point(350, 211);
+            this.btn_chair08.Name = "btn_chair08";
+            this.btn_chair08.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair08.TabIndex = 7;
+            this.btn_chair08.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair07
+            // 
+            this.btn_chair07.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair07.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair07.BackgroundImage")));
+            this.btn_chair07.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair07.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair07.Location = new System.Drawing.Point(250, 211);
+            this.btn_chair07.Name = "btn_chair07";
+            this.btn_chair07.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair07.TabIndex = 6;
+            this.btn_chair07.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair06
+            // 
+            this.btn_chair06.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair06.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair06.BackgroundImage")));
+            this.btn_chair06.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair06.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair06.Location = new System.Drawing.Point(150, 211);
+            this.btn_chair06.Name = "btn_chair06";
+            this.btn_chair06.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair06.TabIndex = 5;
+            this.btn_chair06.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair15
+            // 
+            this.btn_chair15.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair15.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair15.BackgroundImage")));
+            this.btn_chair15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair15.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair15.Location = new System.Drawing.Point(550, 303);
+            this.btn_chair15.Name = "btn_chair15";
+            this.btn_chair15.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair15.TabIndex = 14;
+            this.btn_chair15.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair14
+            // 
+            this.btn_chair14.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair14.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair14.BackgroundImage")));
+            this.btn_chair14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair14.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair14.Location = new System.Drawing.Point(450, 303);
+            this.btn_chair14.Name = "btn_chair14";
+            this.btn_chair14.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair14.TabIndex = 13;
+            this.btn_chair14.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair13
+            // 
+            this.btn_chair13.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair13.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair13.BackgroundImage")));
+            this.btn_chair13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair13.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair13.Location = new System.Drawing.Point(350, 303);
+            this.btn_chair13.Name = "btn_chair13";
+            this.btn_chair13.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair13.TabIndex = 12;
+            this.btn_chair13.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair12
+            // 
+            this.btn_chair12.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair12.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair12.BackgroundImage")));
+            this.btn_chair12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair12.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair12.Location = new System.Drawing.Point(250, 303);
+            this.btn_chair12.Name = "btn_chair12";
+            this.btn_chair12.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair12.TabIndex = 11;
+            this.btn_chair12.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair11
+            // 
+            this.btn_chair11.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair11.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair11.BackgroundImage")));
+            this.btn_chair11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair11.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair11.Location = new System.Drawing.Point(150, 303);
+            this.btn_chair11.Name = "btn_chair11";
+            this.btn_chair11.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair11.TabIndex = 10;
+            this.btn_chair11.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair20
+            // 
+            this.btn_chair20.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair20.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair20.BackgroundImage")));
+            this.btn_chair20.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair20.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair20.Location = new System.Drawing.Point(550, 397);
+            this.btn_chair20.Name = "btn_chair20";
+            this.btn_chair20.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair20.TabIndex = 19;
+            this.btn_chair20.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair19
+            // 
+            this.btn_chair19.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair19.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair19.BackgroundImage")));
+            this.btn_chair19.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair19.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair19.Location = new System.Drawing.Point(450, 397);
+            this.btn_chair19.Name = "btn_chair19";
+            this.btn_chair19.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair19.TabIndex = 18;
+            this.btn_chair19.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair18
+            // 
+            this.btn_chair18.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair18.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair18.BackgroundImage")));
+            this.btn_chair18.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair18.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair18.Location = new System.Drawing.Point(350, 397);
+            this.btn_chair18.Name = "btn_chair18";
+            this.btn_chair18.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair18.TabIndex = 17;
+            this.btn_chair18.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair17
+            // 
+            this.btn_chair17.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair17.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair17.BackgroundImage")));
+            this.btn_chair17.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair17.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair17.Location = new System.Drawing.Point(250, 397);
+            this.btn_chair17.Name = "btn_chair17";
+            this.btn_chair17.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair17.TabIndex = 16;
+            this.btn_chair17.UseVisualStyleBackColor = false;
+            // 
+            // btn_chair16
+            // 
+            this.btn_chair16.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chair16.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_chair16.BackgroundImage")));
+            this.btn_chair16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_chair16.ForeColor = System.Drawing.Color.Transparent;
+            this.btn_chair16.Location = new System.Drawing.Point(150, 397);
+            this.btn_chair16.Name = "btn_chair16";
+            this.btn_chair16.Size = new System.Drawing.Size(74, 71);
+            this.btn_chair16.TabIndex = 15;
+            this.btn_chair16.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btn_chair20);
+            this.Controls.Add(this.btn_chair19);
+            this.Controls.Add(this.btn_chair18);
+            this.Controls.Add(this.btn_chair17);
+            this.Controls.Add(this.btn_chair16);
+            this.Controls.Add(this.btn_chair15);
+            this.Controls.Add(this.btn_chair14);
+            this.Controls.Add(this.btn_chair13);
+            this.Controls.Add(this.btn_chair12);
+            this.Controls.Add(this.btn_chair11);
+            this.Controls.Add(this.btn_chair10);
+            this.Controls.Add(this.btn_chair09);
+            this.Controls.Add(this.btn_chair08);
+            this.Controls.Add(this.btn_chair07);
+            this.Controls.Add(this.btn_chair06);
+            this.Controls.Add(this.btn_chair05);
+            this.Controls.Add(this.btn_chair04);
+            this.Controls.Add(this.btn_chair03);
+            this.Controls.Add(this.btn_chair02);
+            this.Controls.Add(this.btn_chair01);
             this.Name = "Form1";
             this.Text = "Sinema Otomasyonu";
             this.ResumeLayout(false);
@@ -43,6 +325,27 @@ namespace SinemaOtomasyonu
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_chair01;
+        private System.Windows.Forms.Button btn_chair02;
+        private System.Windows.Forms.Button btn_chair03;
+        private System.Windows.Forms.Button btn_chair04;
+        private System.Windows.Forms.Button btn_chair05;
+        private System.Windows.Forms.Button btn_chair10;
+        private System.Windows.Forms.Button btn_chair09;
+        private System.Windows.Forms.Button btn_chair08;
+        private System.Windows.Forms.Button btn_chair07;
+        private System.Windows.Forms.Button btn_chair06;
+        private System.Windows.Forms.Button btn_chair15;
+        private System.Windows.Forms.Button btn_chair14;
+        private System.Windows.Forms.Button btn_chair13;
+        private System.Windows.Forms.Button btn_chair12;
+        private System.Windows.Forms.Button btn_chair11;
+        private System.Windows.Forms.Button btn_chair20;
+        private System.Windows.Forms.Button btn_chair19;
+        private System.Windows.Forms.Button btn_chair18;
+        private System.Windows.Forms.Button btn_chair17;
+        private System.Windows.Forms.Button btn_chair16;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,43 @@ namespace SinemaOtomasyonu
         {
             InitializeComponent();
         }
+
+        int chair01 = 0;
+        int chair02 = 0;
+        int chair03 = 0;
+        int chair04 = 0;
+        int chair05 = 0;
+        int chair06 = 0;
+        int chair07 = 0;
+        int chair08 = 0;
+        int chair09 = 0;
+        int chair10 = 0;
+        int chair11 = 0;
+        int chair12 = 0;
+        int chair13 = 0;
+        int chair14 = 0;
+        int chair15 = 0;
+        int chair16 = 0;
+        int chair17 = 0;
+        int chair18 = 0;
+        int chair19 = 0;
+        int chair20 = 0;
+
+        private void btn_chair01_Click(object sender, EventArgs e)
+        {
+           
+            if (chair01 == 0)
+            {
+                 btn_chair01.BackgroundImage = System.Drawing.Image.FromFile(@"C:\Users\ahmet\source\repos\SinemaOtomasyonu\Sinema-Otomasyonu\images\redchair.png");
+                 chair01 = 1;
+            } 
+
+            else if (chair01==1)
+            { 
+                btn_chair01.BackgroundImage = System.Drawing.Image.FromFile(@"C:\Users\ahmet\source\repos\SinemaOtomasyonu\Sinema-Otomasyonu\images\greenchair.png");
+                chair01 = 0;
+            }
+
+        }
     }
 }
